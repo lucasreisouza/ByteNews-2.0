52ab25c69a16a019ec3a98c226a8cf1d8afc61a8
Luís Henrique Moreira Araújo <luismaraujo28@gmail.com>
2026-04-23 17:37:39 -0300

    ByteNews

--- src/scripts/script-index.js
+++ src/scripts/script-index.js
@@ -1,15 +1,20 @@
 const body = document.querySelector('body');
-const logo = document.querySelector('#logo');
-const tema = document.querySelector('#tema')
+const logoHeader = document.querySelector('#logoHeader');
+const logoFooter = document.querySelector('#logoFooter');
+const temaIcon = document.querySelector('#iconTema');
 
 function toggleStyle() {
   if (body.classList.contains('dark')) {
     body.classList.remove('dark');
     body.classList.add('clear');
-    logo.setAttribute('src', './src/assets/icons/logo-claro.png');
+    logoHeader.src = './src/assets/icons/logo-claro.png';
+    logoFooter.src = './src/assets/icons/logo-claro.png';
+    temaIcon.src = './src/assets/icons/moon.png';
   } else {
     body.classList.remove('clear');
     body.classList.add('dark');
-    logo.setAttribute('src', './src/assets/icons/logo-padrao.png');
+    logoHeader.src = './src/assets/icons/logo-padrao.png';
+    logoFooter.src = './src/assets/icons/logo-padrao.png';
+    temaIcon.src = './src/assets/icons/sun.png';
   }
 }
